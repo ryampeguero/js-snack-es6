9119075d0e0f94cf74533fde8353c66131717312
ryampeguero <ryampeguero@gmail.com>
2024-04-16 12:35:52 +0200

    feat: added a new way to reach the goal with .map method

--- jsnack-4/js/script.js
+++ jsnack-4/js/script.js
@@ -24,19 +24,31 @@ const persone = [
 
 ]
 
-const newArray = [];
+// const newArray = [];
 
-persone.forEach((currElem, i)=>{
+// persone.forEach((currElem, i)=>{
+//     const {nome, cognome, eta} = currElem;
+//     newArray[i] = `${nome} ${cognome} può guidare? ${puoGuidare(eta)}`
+// });
+
+// console.log(newArray);
+
+// function puoGuidare(age){
+//     if(age < 18 ){
+//         return "No è minorenne";
+//     }else if(age >= 18){
+//         return "Si è maggiorenno";
+//     }
+// }
+
+//OR
+
+const newArray = persone.map((currElem)=>{
     const {nome, cognome, eta} = currElem;
-    newArray[i] = `${nome} ${cognome} può? guidare ${puoGuidare(eta)}`
-});
+    if(eta < 18)
+        return `${nome} ${cognome} può guidare? No è minorenne`
+    else
+        return `${nome} ${cognome} può guidare? Si è maggiorenne`
+})
 
 console.log(newArray);
-
-function puoGuidare(age){
-    if(age < 18 ){
-        return "No è minorenne";
-    }else if(age >= 18){
-        return "Si è maggiorenno";
-    }
-}
